5b0f6f2131a709cf4a4218ce1f3fe6559d4f240c
Shreeya Sethi <67246350+shreeyasethi@users.noreply.github.com>
2025-03-21 22:23:09 -0400

    Update ui.ts6

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `Hi! I'm Darren. What event are we pl
 export const FOOTER_MESSAGE: string = `Customized AI event planner`;
 export const CLEAR_BUTTON_TEXT: string = `[Clear Chat]`;
 export const PAGE_TITLE: string = `Event Planning with Darren`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME} to plan a party!`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
