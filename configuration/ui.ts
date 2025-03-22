f119d19aa99bc29ec75355caa7fd31eec88335cc
Shreeya Sethi <67246350+shreeyasethi@users.noreply.github.com>
2025-03-21 22:12:35 -0400

    Update ui.ts3

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,9 @@
 import {Darren, ShreeyaSethi } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `AI Event Planner`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Hi! I'm Darren. What event are we planning today?`;
 export const FOOTER_MESSAGE: string = `Customized AI event planner`;
-export const CLEAR_BUTTON_TEXT: string = `Hi! I'm Darren. What event are we planning today?`;
+export const CLEAR_BUTTON_TEXT: string = `[Clear Chat]`;
 export const PAGE_TITLE: string = `Event Planning with Darren`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
