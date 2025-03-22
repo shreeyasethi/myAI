fdf9288be6adadfbe9921041b0168bc263adf67f
Shreeya Sethi <67246350+shreeyasethi@users.noreply.github.com>
2025-03-21 22:25:43 -0400

    Update identity.ts7

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[Shreeya Sethi]`;
-export const OWNER_DESCRIPTION: string = `[Student at UNC creating an event planner AI.]`;
+export const OWNER_NAME: string = `Shreeya Sethi`;
+export const OWNER_DESCRIPTION: string = `Student at UNC creating an event planner AI.`;
 
-export const AI_NAME: string = `[Darren]`;
+export const AI_NAME: string = `Darren`;
 export const AI_TONE: string = `[Helpful]`;
-export const AI_ROLE: string = `[event planner]`;
+export const AI_ROLE: string = `event planner`;
